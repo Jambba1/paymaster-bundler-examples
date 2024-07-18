905aeae051dd488c316da17cdc9e6468b2fcd169
Jambba1 <salminen.jami@gmail.com>
2024-07-18 17:50:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export default {
     "rpc_url": process.env.RPC_URL,
     "private_key": process.env.PRIVATE_KEY,
     "account_type": process.env.ACCOUNT_TYPE || 'simple',
-    "contract_address": "0x66519FCAee1Ed65bc9e0aCc25cCD900668D3eD49",
+    "contract_address": "0x4e888104F2a10Fc82Ac7fc903cC0f66c472018DC",
     "function_name": "mintTo",
-    "entry_point": "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+    "entry_point": "0x24b76E6DE17b928dE889c2aDe037e15D452B1291"
 }
